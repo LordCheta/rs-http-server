b2da4e99fa1bec66fbfd45087b739ac9534b305d
yvcheta <cheta@youverify.co>
2022-01-23 22:42:33 +0100

    wip: constructing query params

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 pub enum Method {
     GET,
     POST,
@@ -9,3 +11,25 @@ pub enum Method {
     CONNECT,
     OPTIONS
 }
+
+impl FromStr for Method {
+    type Err = MethodError; 
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+
+        match s {
+            "GET" => Ok(Self::GET),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "PATCH" => Ok(Self::PATCH),
+            "DELETE" => Ok(Self::DELETE),
+            "TRACE" => Ok(Self::TRACE),
+            "HEAD" => Ok(Self::HEAD),
+            "CONNECT" => Ok(Self::CONNECT),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            _ => Err(MethodError)
+        }
+    }
+}
+
+pub struct MethodError;
