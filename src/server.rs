13174c06b470ec9459d7e9cfb18f0aa356e8164f
yvcheta <cheta@youverify.co>
2022-01-23 12:07:41 +0100

    wip: parsing recieved reques

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
+use crate::http::Request;
+use std::convert::TryFrom;
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
     address: String
@@ -12,8 +15,38 @@ impl Server {
     }
 
     pub fn run(self) {
+
+        let listener = TcpListener::bind(&self.address).unwrap();
+
         println!("Server started on: {}", self.address);
 
-        let listener = TcpListener::bind(&self.address);
+        loop {
+
+            match listener.accept() {
+
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Recieved a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {
+                                    
+                                },
+
+                                Err(e) => println!("Failed to parse request with error: {}", e),
+                            }
+                        }
+
+                        Err(e) => println!("Failed to read from connection with error: {}", e),
+                    }
+                },
+
+                Err(e) => println!("Failed to connect with error: {}", e),
+
+            }
+        }
     }
 }
